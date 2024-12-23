0c33d334f58c291b421eadcd2abbec3404ecaf7c
eunice0035 <eunice7357@gmail.com>
2024-12-23 21:34:29 +0900

    feat: add UI logic

---
+++ src/main/kotlin/lotto/ui/ResultView.kt
@@ -0,0 +1,21 @@
+package lotto.ui
+
+import lotto.domain.Lotto
+
+class ResultView {
+    fun printTickets(tickets: List<Lotto>) {
+        tickets.forEach { println(it.numbers) }
+    }
+
+    fun printResults(
+        results: Map<Int, Int>,
+        profit: Double,
+    ) {
+        println("당첨 통계")
+        println("---------")
+        results.forEach { (matchCount, count) ->
+            println("$matchCount 개 일치 - $count 개")
+        }
+        println("총 수익률은 $profit 입니다.")
+    }
+}
