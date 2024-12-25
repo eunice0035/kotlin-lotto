1aff168dfde7eceaeeafb0d871e4254b39d99e72
eunice0035 <eunice7357@gmail.com>
2024-12-25 22:45:48 +0900

    refactor: refactor Lotto Maker

--- src/main/kotlin/lotto/domain/LottoMaker.kt
+++ src/main/kotlin/lotto/domain/LottoMaker.kt
@@ -1,13 +1,17 @@
 package lotto.domain
 
-class LottoMaker {
+class LottoMaker(private val numberGenerator: () -> List<Int>) {
     fun makeLotto(): Lotto {
-        return Lotto((START_NUMBER..END_NUMBER).shuffled().take(LOTTO_COUNT).sorted())
+        return Lotto(numberGenerator().sorted())
     }
 
     companion object {
-        const val START_NUMBER = 1
-        const val END_NUMBER = 45
-        const val LOTTO_COUNT = 6
+        private const val START_NUMBER = 1
+        private const val END_NUMBER = 45
+        private const val LOTTO_COUNT = 6
+
+        fun defaultMaker(): LottoMaker {
+            return LottoMaker({ (START_NUMBER..END_NUMBER).shuffled().take(LOTTO_COUNT).sorted() })
+        }
     }
 }
--- src/test/kotlin/lotto/domain/LottoMakerTest.kt
+++ src/test/kotlin/lotto/domain/LottoMakerTest.kt
@@ -1,20 +1,31 @@
 package lotto.domain
 
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
 
 class LottoMakerTest {
     @Test
-    fun `Lotto - size shoule be 6`() {
-        val lottoMaker = LottoMaker()
+    fun `Lotto - size should be 6`() {
+        val fixedNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val lottoMaker = LottoMaker { fixedNumbers }
         val lotto = lottoMaker.makeLotto()
-        assertEquals(6, lotto.numbers.size)
+
+        lotto.numbers.size shouldBe 6
     }
 
     @Test
-    fun `Lotto - shoule be in range(1-45)`() {
-        val lottoMaker = LottoMaker()
+    fun `Lotto - should be same with list`() {
+        val fixedNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val lottoMaker = LottoMaker { fixedNumbers }
         val lotto = lottoMaker.makeLotto()
-        assertTrue(lotto.numbers.all { it in 1..45 })
+
+        lotto.numbers shouldBe fixedNumbers
+    }
+
+    @Test
+    fun `Lotto - should be in range(1-45)`() {
+        val lottoMaker = LottoMaker.defaultMaker()
+        val lotto = lottoMaker.makeLotto()
+        lotto.numbers.all { it in 1..45 } shouldBe true
     }
 }
